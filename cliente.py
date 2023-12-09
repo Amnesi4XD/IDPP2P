5f54e248b6416e5a4b2c475f0631188979927802
Rocha <lucasrocha216.lr@gmail.com>
2023-12-09 17:18:14 -0300

    continuando implementação

--- cliente.py
+++ cliente.py
@@ -1,14 +1,55 @@
 #!/usr/bin/python
 
+import secrets
+import string
+import sys
 import time
 import socket
 from _thread import *
+import os
+from hashlib import md5
 
 porta_tcp = None
 
-def configurar_ambiente():
-    pass
+informacao_cliente = {}
 
+def gerar_senha():
+    tamanho = 8
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(secrets.choice(caracteres) for _ in range(tamanho))
+    return senha
+
+
+
+def listar_arquivos():
+    lista = []
+    for arquivo in os.listdir(informacao_cliente['nome_diretorio']):
+        md5 = md5(open(arquivo, 'rb').read()).hexdigest()
+        elemento_lista = md5 + ',' + arquivo
+        lista.append(elemento_lista)
+    return lista
+def controle_udp(diretorio):
+    global porta_tcp
+    lista_arquivos = lista_arquivos(diretorio)
+
+def configurar_ambiente():
+    #Pegando porta disponível
+    porta_tcp = descobre_porta_disponivel()
+    informacao_cliente['porta'] = porta_tcp
+    
+    #Gerando senha para conexão udp
+    senha = gerar_senha()
+    informacao_cliente['senha'] = senha
+    
+    #Checando path do diretório de arquivos
+    if  not os.path.isdir(informacao_cliente['nome_diretorio']):
+        print('Diretorio para salvar arquivos não existe, gostaria de criar diretório com esse nome?')    
+        resposta = input('S/N: ')
+        if resposta == 'S':
+            os.mkdir(informacao_cliente['nome_diretorio'])
+        else:
+            print('Encerrando serviço')
+            sys.exit(0)
 
 def descobre_porta_disponivel():
     # Define a porta inicial
@@ -16,56 +57,77 @@ def descobre_porta_disponivel():
 
     while True:
         try:
-            # Tenta criar um socket na porta especificada
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.bind(('localhost', porta_inicial))
-            # Se bem-sucedido, retorna a porta
-            return porta_inicial
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind(('localhost', porta_inicial))
+            # Se bem-sucedido, fecha o socket
+            s.close()
+            # Retorna a porta
+            porta_inicial += 1
         except socket.error:
             # Se a porta estiver em uso, tenta a próxima
-            porta_inicial += 1
+            return porta_inicial
 
-def controle_udp():
-    # Código do serviço UDP
+
+def controle_udp(senha, socket_cliente, endereco_servidor):
     while True:
-        print('Controle UDP funcionando')
-        time.sleep(5)
+        #cria interfaçe para o usúario poder selecionar se ele quer listar arquivos disponíveis, baixar um arquivo ou sair do programa
+        print('Selecione uma opção:')
+        print('1 - Atualizar arquivos disponíveis')
+        print('2 - Baixar um arquivo')
+        print('3 - Sair')
+        opcao = input('Opção: ')
+        if opcao == '1':
+            try:
+                socket_cliente.sendto(f"UPD {informacao_cliente['porta_tcp']}", endereco_servidor)
+                data, server = socket_cliente.recvfrom(4096)
+                print(data.decode('utf-8'))
+            except:
+                print('Erro ao tentar conectar no servidor')
+                sys.exit(0)
 
-
-def servico_tcp(client):
+def servico_tcp(client, diretorio):
     # Código do serviço TCP
     print('Nova conexao TCP')
     client.send(b'OI')
     client.close()
 
 
-def controle_tcp():
-    global porta_tcp
+def controle_tcp(senha):
     _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     _socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    _socket.bind(('', porta_tcp))
+    _socket.bind(('', informacao_cliente['porta_tcp']))
     _socket.listen(4096)
     while True:
         client, addr = _socket.accept()
         start_new_thread(servico_tcp, (client, ))
+        
 
 
-def inicia_controle_tcp():
-    controle_tcp()
+def inicia_controle_tcp(diretorio):
+    controle_tcp(diretorio)
 
 
 def inicia_controle_udp():
+    # Inicia a conexão UDP
+    socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    endereco_servidor = ('localhost', 54494)
+    
+    #Retirar após testes
+    try:   
+        socket_cliente.sendto(f"REG {informacao_cliente['senha']} {porta_tcp} {listar_arquivos()}", endereco_servidor)
+    except:
+        print('Erro ao tentar conectar no servidor')
+        sys.exit(0)
     controle_udp()
 
 
-def main():
-    global porta_tcp
-    porta_tcp = descobre_porta_disponivel()
-
+def main():    
     configurar_ambiente()
+    
 
-    start_new_thread(inicia_controle_tcp, ())
-    start_new_thread(inicia_controle_udp, ())
+    start_new_thread(inicia_controle_tcp(), ())
+    start_new_thread(inicia_controle_udp(), ())
 
     while True:
         time.sleep(60)
@@ -73,4 +135,10 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 2:
+        informacao_cliente['ip'] = sys.argv[1]
+        informacao_cliente['nome_diretorio'] = sys.argv[2]
+        main()
+    else:
+        print('Uso: python cliente.py <ip> <diretorio>')
+        sys.exit(0)
